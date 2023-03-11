a2342691dc43be53ba46ba088273ebc0416b1911
Namra Patel <namrajpatel@gmail.com>
2023-03-11 11:04:30 -0500

    upload works

--- src/upload.cpp
+++ src/upload.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
 #include <curl/curl.h>
 
 size_t upload_callback(char* buffer, size_t size, size_t nitems, void* userdata)
@@ -16,7 +17,7 @@ int main()
         return 1;
     }
 
-    std::string file_path = "./data/5MB.bin";
+    std::string file_path = "./data/10MB.zip";
     std::ifstream file(file_path, std::ios::binary);
     if (!file)
     {
@@ -37,25 +38,40 @@ int main()
     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, file_size);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, upload_callback);
 
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
+    int num_trials = 5;
+    double total_speed = 0.0;
+    for (int i = 0; i < num_trials; i++)
     {
-        std::cerr << "Error uploading file: " << curl_easy_strerror(res) << std::endl;
-    }
-    else
-    {
-        long response_code;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+        auto start_time = std::chrono::steady_clock::now();
+        CURLcode res = curl_easy_perform(curl);
+        auto end_time = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-        if (response_code == 200)
+        if (res != CURLE_OK)
         {
-            std::cout << "Upload completed successfully" << std::endl;
+            std::cerr << "Error uploading file: " << curl_easy_strerror(res) << std::endl;
         }
         else
         {
-            std::cerr << "Error: unexpected response code " << response_code << std::endl;
+            long response_code;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+
+            if (response_code == 200)
+            {
+                // upload speed = (file size / duration) * 1000 ms / 
+                double upload_speed = (file_size / (double)duration.count()) * 1000.0 / (1024.0 * 1024.0); 
+                std::cout << "Trial " << i + 1 << " upload speed: " << upload_speed << " MB/s" << std::endl;
+                total_speed += upload_speed;
+            }
+            else
+            {
+                std::cerr << "Error: unexpected response code " << response_code << std::endl;
+            }
         }
     }
+
+    double average_speed = total_speed / num_trials;
+    std::cout << "Average upload speed: " << average_speed << " MB/s" << std::endl;
 
     curl_easy_cleanup(curl);
     delete[] buffer;
