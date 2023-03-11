7497d5d1776ce1232b2adb53dd96b68c4bbd0b97
Namra Patel <namrajpatel@gmail.com>
2023-03-11 10:29:41 -0500

    add test data, upload.cpp draft

--- src/upload.cpp
+++ src/upload.cpp
@@ -1,57 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <chrono>
 #include <curl/curl.h>
 
-using namespace std;
+size_t upload_callback(char* buffer, size_t size, size_t nitems, void* userdata)
+{
+    return size * nitems;
+}
 
 int main()
 {
-    CURL *curl;
-    CURLcode res;
-    FILE *fp;
-    long http_code;
-    string url = "http://www.example.com/upload.php"; // change this to your server's upload script URL
-    string filename = "20MB.bin"; // change this to the name of the file you want to upload
-    int file_size_mb = 20; // change this to the size of the file you want to upload, in megabytes
-
-    // generate a file with random data
-    ofstream fout(filename, ios::binary);
-    for (int i = 0; i < file_size_mb * 1024 * 1024; i++) {
-        fout << (char)(rand() % 256);
+    CURL* curl = curl_easy_init();
+    if (!curl)
+    {
+        std::cerr << "Error initializing curl" << std::endl;
+        return 1;
     }
-    fout.close();
 
-    // upload the file using libcurl
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
-    if (curl) {
-        fp = fopen(filename.c_str(), "rb");
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-        curl_easy_setopt(curl, CURLOPT_READDATA, fp);
-        curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_size_mb * 1024 * 1024);
-        auto start_time = chrono::high_resolution_clock::now(); // declare start_time here
-        res = curl_easy_perform(curl);
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-        curl_easy_cleanup(curl);
-        fclose(fp);
-        auto end_time = chrono::high_resolution_clock::now();
-        auto elapsed_time_ms = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
-        double upload_speed_mbps = (double)file_size_mb / ((double)elapsed_time_ms / 1000.0);
-        cout << "Upload speed: " << upload_speed_mbps << " Mbps" << endl;
+    std::string file_path = "./data/20MB.bin";
+    std::ifstream file(file_path, std::ios::binary);
+    if (!file)
+    {
+        std::cerr << "Error opening file: " << file_path << std::endl;
+        return 1;
     }
-    curl_global_cleanup();
 
-    // delete the temporary file
-    // remove(filename.c_str());
+    file.seekg(0, std::ios::end);
+    long long file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-    // print the result
-    if (res != CURLE_OK) {
-        cerr << "Error: " << curl_easy_strerror(res) << endl;
-    } else if (http_code != 200) {
-        cerr << "HTTP error code: " << http_code << endl;
+    char* buffer = new char[file_size];
+    file.read(buffer, file_size);
+
+    printf("1");
+    curl_easy_setopt(curl, CURLOPT_URL, "ftp://tele2.net/upload/");
+    curl_easy_setopt(curl, CURLOPT_USERPWD, "anonymous:anonymous");
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+    curl_easy_setopt(curl, CURLOPT_READDATA, buffer);
+    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, file_size);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, upload_callback);
+    printf("2");
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+        std::cerr << "Error uploading file: " << curl_easy_strerror(res) << std::endl;
     }
+    printf("3");
+    curl_easy_cleanup(curl);
+    delete[] buffer;
 
     return 0;
 }
